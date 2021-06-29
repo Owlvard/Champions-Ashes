d8470bbf0d1f5192a018c41047948ec4d38f6854
Halvard <sirhalvard@gmail.com>
2021-06-29 21:01:44 +0200

    patch 1.6

---
+++ GraphicPresets/Enable FIDELITY.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+echo     ___         __                 _______     __     ___ __       
+echo    /   ^|  _____/ /_  ___  _____   / ____(_)___/ /__  / (_) /___  __
+echo   / /^| ^| / ___/ __ \/ _ \/ ___/  / /_  / / __  / _ \/ / / __/ / / /
+echo  / ___ ^|(__  ) / / /  __(__  )  / __/ / / /_/ /  __/ / / /_/ /_/ / 
+echo /_/  ^|_/____/_/ /_/\___/____/  /_/   /_/\__,_/\___/_/_/\__/\__, /  
+echo                                                           /____/   
+echo.
+ECHO [EXPERIMENTAL, SOME GLITCHES MIGHT OCCUR]
+ECHO Amazing visuals or burning PC, either way you will see the light!
+ECHO.
+ECHO Initiating Preset...
+robocopy ".\Fidelity\map" "..\_ashes\map" "*.tpf*" "*.btl*" "*.msb*" /e /z /mir >nul 2>nul
+robocopy ".\Fidelity\param\drawparam" "..\_ashes\param\drawparam" "*.gparam*" /e /z /mir >nul 2>nul
+ECHO READY, Enjoy Ashes!
+PAUSE
---
+++ GraphicPresets/Enable PERFORMANCE.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+ECHO.
+ECHO  _____     _              _____         ___                               
+ECHO ^|  _  ^|___^| ^|_ ___ ___   ^|  _  ^|___ ___^|  _^|___ ___ _____ ___ ___ ___ ___ 
+ECHO ^|     ^|_ -^|   ^| -_^|_ -^|  ^|   __^| -_^|  _^|  _^| . ^|  _^|     ^| .'^|   ^|  _^| -_^|
+ECHO ^|__^|__^|___^|_^|_^|___^|___^|  ^|__^|  ^|___^|_^| ^|_^| ^|___^|_^| ^|_^|_^|_^|__,^|_^|_^|___^|___^|
+ECHO.
+ECHO Improved visuals with minimal performance loss
+ECHO.
+ECHO Initiating Preset...
+robocopy ".\Performance\map" "..\_ashes\map" "*.tpf*" "*.btl*" "*.msb*" /e /z /mir >nul 2>nul
+robocopy ".\Performance\param\drawparam" "..\_ashes\param\drawparam" "*.gparam*" /e /z /mir >nul 2>nul
+ECHO READY, Enjoy Ashes!
+PAUSE
