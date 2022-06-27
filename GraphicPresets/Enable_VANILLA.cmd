7ed84d131c5f7caf890705d37d91211ec361d1ed
Angeluso <angelosanada@gmail.com>
2022-06-27 19:02:00 +0200

    2.2

---
+++ GraphicPresets/Enable_VANILLA.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+echo  __ __   ____  ____   ____  _      _       ____ 
+echo ^|  ^|  ^| /    ^|^|    \ ^|    ^|^| ^|    ^| ^|     /    ^|
+echo ^|  ^|  ^|^|  o  ^|^|  _  ^| ^|  ^| ^| ^|    ^| ^|    ^|  o  ^|
+echo ^|  ^|  ^|^|     ^|^|  ^|  ^| ^|  ^| ^| ^|___ ^| ^|___ ^|     ^|
+echo ^|  :  ^|^|  _  ^|^|  ^|  ^| ^|  ^| ^|     ^|^|     ^|^|  _  ^|
+echo  \   / ^|  ^|  ^|^|  ^|  ^| ^|  ^| ^|     ^|^|     ^|^|  ^|  ^|
+echo   \_/  ^|__^|__^|^|__^|__^|^|____^|^|_____^|^|_____^|^|__^|__^|
+echo.
+ECHO unchanged visuals
+ECHO.
+ECHO Initiating Preset...
+robocopy ".\Vanilla\map" "..\_ashes\map" "*.tpf*" "*.txt*" "*.btl*" "*.msb*" "*.dcx*" /e /z /mir >nul 2>nul
+robocopy ".\Vanilla\param\drawparam" "..\_ashes\param\drawparam" "*.gparam*" /e /z /mir >nul 2>nul
+ECHO READY, Enjoy Ashes!
+PAUSE
